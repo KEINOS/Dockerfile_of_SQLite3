788c1ec8fb703b99e2ebc2d1235a8e1bf6e68b54
KEINOS <github@keinos.com>
2020-12-19 11:39:49 +0900

    Fix Dockle vulnerability warning (#9)
    * fix: #6 CIS-DI-0001 Last user should not be root so set to user "`sqlite`".
    * fix: #7 CIS-DI-0005 Enable content trust for Docker while analyzing in GitHub Actions.
    * fix: #8 CIS-DI-0006 Add HEALTHCHECK instruction to the container image
    * chore: Add comment and some cleaning up
    * docs: Add status badge and fix document

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,51 @@
 FROM keinos/alpine:latest AS build-env
 
 COPY run-test.sh /run-test.sh
-RUN apk add --update \
-      alpine-sdk \
-      build-base  \
-      tcl-dev \
-      tk-dev \
-      mesa-dev \
-      jpeg-dev \
-      libjpeg-turbo-dev \
-    && wget \
-      -O sqlite.tar.gz \
-      https://www.sqlite.org/src/tarball/sqlite.tar.gz?r=release \
-    && tar xvfz sqlite.tar.gz \
-    && ./sqlite/configure --prefix=/usr \
-    && make \
-    && make install \
-    && sqlite3 --version \
-    && /run-test.sh
 
+RUN \
+  apk add --update \
+    alpine-sdk \
+    build-base  \
+    tcl-dev \
+    tk-dev \
+    mesa-dev \
+    jpeg-dev \
+    libjpeg-turbo-dev \
+  # Download latest release
+  && wget \
+    -O sqlite.tar.gz \
+    https://www.sqlite.org/src/tarball/sqlite.tar.gz?r=release \
+  && tar xvfz sqlite.tar.gz \
+  # Configure and make SQLite3 binary
+  && ./sqlite/configure --prefix=/usr \
+  && make \
+  && make install \
+  # Smoke test
+  && sqlite3 --version \
+  && /run-test.sh
+
+# -----------------------------------------------------------------------------
 FROM keinos/alpine:latest
+
 COPY --from=build-env /usr/bin/sqlite3 /usr/bin/sqlite3
 COPY run-test.sh /run-test.sh
-CMD sqlite3
+
+# Create a group and user for SQLite3 to avoid: Dockle CIS-DI-0001
+ENV \
+  USER_SQLITE=sqlite \
+  GROUP_SQLITE=sqlite
+
+RUN addgroup -S $GROUP_SQLITE \
+  && adduser  -S $USER_SQLITE -G $GROUP_SQLITE
+
+USER $USER_SQLITE
+
+# Set container's default command as `sqlite3`
+CMD /usr/bin/sqlite3
+
+# Avoid: Dockle CIS-DI-0006
+HEALTHCHECK \
+  --start-period=1m \
+  --interval=5m \
+  --timeout=3s \
+  CMD /usr/bin/sqlite3 --version || exit 1
--- check-updates.sh
+++ check-updates.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
-
 # This script checks if there's an update in SQLite3 then commit/push the changes.
-#
 # Exits with "1"(false) if no update found. If update found then
 # it will update and exits with "0"(true) when no error occurs.
 
+set -eu
+
 # Create random image name for testing
-NAME_TAG_IMAGE='test-'$(cat /dev/urandom | base64 | tr -cd 'a-z0-9' | head -c 10)
-echo "- Test image name: ${NAME_TAG_IMAGE}"
+NAME_TAG_IMAGE="test-$(base64 </dev/urandom | tr -cd 'a-z0-9' | head -c 10)"
+echo "- Name of test image: ${NAME_TAG_IMAGE}"
 
 # Include previous version of SQLite3
-source VERSION_SQLite3.txt
+source ./VERSION_SQLite3.txt
 echo "- Previous version was: ${VERSION_SQLITE3}"
 
 # Build and run container to get the version
-docker build --tag $NAME_TAG_IMAGE . && \
-VERSION_SQLITE3_LATEST=$(docker run --rm $NAME_TAG_IMAGE sqlite3 --version) && \
-docker container prune -f && \
-docker image rm $NAME_TAG_IMAGE -f && \
-docker image prune -f
-if [ $? -ne 0 ]; then
+docker build --tag "$NAME_TAG_IMAGE" . &&
+  VERSION_SQLITE3_LATEST=$(docker run --rm "$NAME_TAG_IMAGE" sqlite3 --version) &&
+  docker container prune -f &&
+  docker image rm "$NAME_TAG_IMAGE" -f &&
+  docker image prune -f
+if test $? -ne 0; then
   echo '- Fail to build or run container.'
   exit 1
 fi
@@ -32,11 +32,11 @@ if [ "${VERSION_SQLITE3}" = "${VERSION_SQLITE3_LATEST}" ]; then
 fi
 
 # Shor version for commit message
-VERSION_SQLITE3_SHORT=$(echo $VERSION_SQLITE3_LATEST | awk '{print $1}')
+VERSION_SQLITE3_SHORT=$(echo "$VERSION_SQLITE3_LATEST" | awk '{print $1}')
 
 # Git commit/push to origin
 echo '- Update found. Udating ... '
-echo "VERSION_SQLITE3='${VERSION_SQLITE3_LATEST}'" > VERSION_SQLite3.txt
-git add . && \
-git commit -m "feat: v${VERSION_SQLITE3_SHORT}" && \
-git push origin
+echo "VERSION_SQLITE3='${VERSION_SQLITE3_LATEST}'" >VERSION_SQLite3.txt
+git add . &&
+  git commit -m "feat: v${VERSION_SQLITE3_SHORT}" &&
+  git push origin
