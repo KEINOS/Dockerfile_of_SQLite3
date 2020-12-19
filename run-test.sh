42b20d7a461da4b06f4dc56c10129d8c4c6e3904
KEINOS <github@keinos.com>
2020-12-19 10:52:36 +0900

    chore: Add comment and some cleaning up

--- run-test.sh
+++ run-test.sh
@@ -1,20 +1,43 @@
 #!/usr/bin/env sh
 
-which sqlite3
-if [ $? -ne 0 ]; then
+which sqlite3 >/dev/null || {
+  echo 'sqlite3 command not found.' >&2
   exit 1
-fi
+}
 
-cd $(dirname $0);
+name_file_db='test.db'
+path_dir_tmp=$(dirname "$(mktemp -u)")
+path_file_db="${path_dir_tmp}/${name_file_db}"
 
-name_db='test.db'
+rm -f "$path_file_db"
 
-rm -f $name_db
-
-sqlite3 $name_db <<'EOL'
+echo '- Creating test DB:'
+sqlite3 "$path_file_db" <<'HEREDOC'
   create table table_sample(timestamp text, description text);
   insert into table_sample values(datetime("now"),"First sample data. Hoo");
   insert into table_sample values(datetime("now"),"Second sample data. Hoo");
-EOL
+HEREDOC
 
-echo 'select * from table_sample;' | sqlite3 $name_db | grep Hoo && rm $name_db
+sqlite3 "$path_file_db" <<'HEREDOC'
+.header on
+.mode column
+  select rowid, * from table_sample;
+HEREDOC
+
+query='select * from table_sample;'
+echo "- Query to run:"
+echo "$query"
+echo "- Result of query:"
+echo "$query" | sqlite3 "$path_file_db" | grep Hoo
+result=$?
+
+echo '- Test result:'
+if [ "$result" -eq 0 ]; then
+  echo 'success'
+else
+  echo 'failed. Did not return expected output.' >&2
+fi
+
+rm -f "$path_file_db"
+
+exit $result
